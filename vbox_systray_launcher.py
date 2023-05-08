32aaedeaba4aa861c95e3462aab9b501086f6d17
Matthew Morgan <matthew@lifandi.org>
2023-05-08 11:09:50 -0400

    Sloppy job of making this work with Python3 and Gtk3.  This probably ain't gonna work with Gtk4.

--- vbox_systray_launcher.py
+++ vbox_systray_launcher.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # Virtualbox Systray Launcher
 # Copyright 20105 Matthew Morgan <matthew@lifandi.org>
 # to show a list of my virtualbox vm's and let me
@@ -18,8 +18,10 @@
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-import gtk
-import glib
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+from gi.repository import GLib
 import subprocess
 import re
 
@@ -27,33 +29,33 @@ class VboxSystrayLauncher:
     def __init__(self):
         self.vm_list = {}
         self.menu = None
-        self.tray = gtk.StatusIcon()
+        self.tray = Gtk.StatusIcon()
         self.tray.set_from_icon_name('virtualbox')
         self.tray.connect('button_press_event', self.handle_button_press)
-        self.tray.set_tooltip('Virtualbox Systray Launcher')
+        self.tray.set_tooltip_text('Virtualbox Systray Launcher')
 
-        self.menu = gtk.Menu()
+        self.menu = Gtk.Menu()
 
         # create static menu items and add them to a list
         # we can reference when [re]loading dynamic items
         self.static_menuitems = []
 
-        separator_menuitem = gtk.SeparatorMenuItem()
+        separator_menuitem = Gtk.SeparatorMenuItem()
         self.menu.append(separator_menuitem)
         self.static_menuitems.append(separator_menuitem)
 
-        launch_vbox_menuitem = gtk.MenuItem('Launch Virtualbox')
+        launch_vbox_menuitem = Gtk.MenuItem(label='Launch Virtualbox')
         launch_vbox_menuitem.connect('activate', self.launch_vbox)
         self.menu.append(launch_vbox_menuitem)
         self.static_menuitems.append(launch_vbox_menuitem)
 
-        about_menuitem = gtk.MenuItem('About')
+        about_menuitem = Gtk.MenuItem(label='About')
         about_menuitem.connect('activate', self.show_about_dialog)
         self.menu.append(about_menuitem)
         self.static_menuitems.append(about_menuitem)
 
-        quit_menuitem = gtk.MenuItem('Quit')
-        quit_menuitem.connect('activate', gtk.main_quit)
+        quit_menuitem = Gtk.MenuItem(label='Quit')
+        quit_menuitem.connect('activate', Gtk.main_quit)
         self.menu.append(quit_menuitem)
         self.static_menuitems.append(quit_menuitem)
 
@@ -63,7 +65,7 @@ class VboxSystrayLauncher:
         self.reload_dynamic_items()
 
         # set a timer to periodically reload the list
-        glib.timeout_add(10000, self.reload_dynamic_items)
+        GLib.timeout_add(10000, self.reload_dynamic_items)
 
     def reload_dynamic_items(self):
         self.update_vm_list()
@@ -77,8 +79,8 @@ class VboxSystrayLauncher:
                 self.menu.remove(menuitem)
 
         item_index = 0
-        for vm_guid, vm_name in self.vm_list.iteritems():
-            new_menuitem = gtk.MenuItem(vm_name)
+        for vm_guid, vm_name in self.vm_list.items():
+            new_menuitem = Gtk.MenuItem(label=vm_name)
             new_menuitem.show()
             new_menuitem.connect('activate', self.run_vm, vm_guid)
             self.menu.insert(new_menuitem, item_index)
@@ -89,25 +91,25 @@ class VboxSystrayLauncher:
     def update_vm_list(self):
         try :
             self.vm_list = {}
-            vm_text_list = subprocess.check_output(["vboxmanage", "list", "vms"])
+            vm_text_list = subprocess.check_output(["vboxmanage", "list", "vms"], universal_newlines=True)
             vm_lines_list = vm_text_list.splitlines()
             vm_regex = re.compile('"(.*)" ({.*})')
             for line in vm_lines_list:
                 vm_re_match = vm_regex.search(line)
                 if vm_re_match:
                     self.vm_list[vm_re_match.group(2)] = vm_re_match.group(1)
-        except Exception, e:
-            print "Got exception: %s" % e
+        except Exception as e:
+            print("Got exception: %s" % e)
 
     def handle_button_press(self, icon, event):
         self.show_menu(icon, event.button, event.time)
 
     def show_menu(self, icon, event_button, event_time):
-        self.menu.popup(None, None, gtk.status_icon_position_menu,
-            event_button, event_time, self.tray)
+        self.menu.popup(None, None, None, None,
+            event_button, event_time)
 
     def show_about_dialog(self, widget):
-        about_dialog = gtk.AboutDialog()
+        about_dialog = Gtk.AboutDialog()
         about_dialog.set_destroy_with_parent (True)
         about_dialog.set_icon_name ("Virtualbox Systray Launcher")
         about_dialog.set_name('Virtualbox Systray Launcher')
@@ -126,4 +128,4 @@ class VboxSystrayLauncher:
 
 if __name__ == "__main__":
     VboxSystrayLauncher()
-    gtk.main()
+    Gtk.main()
